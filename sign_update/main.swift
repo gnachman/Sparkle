d163511d43fa00d5b661b4f57b5f192b6c35df93
George Nachman <gnachman@gmail.com>
2019-02-02 13:37:31 -0800

    [george] Hack sign_update for my workflow.
    It now prints the signature and takes the key on the command line because I can't get the keychain to work from a launchd job.

--- sign_update/main.swift
+++ sign_update/main.swift
@@ -9,32 +9,11 @@
 import Foundation
 import Security
 
-func findKeys() -> (Data, Data) {
-    var item: CFTypeRef?;
-    let res = SecItemCopyMatching([
-        kSecClass as String: kSecClassGenericPassword,
-        kSecAttrService as String: "https://sparkle-project.org",
-        kSecAttrAccount as String: "ed25519",
-        kSecAttrProtocol as String: kSecAttrProtocolSSH,
-        kSecReturnData as String: kCFBooleanTrue,
-        ] as CFDictionary, &item);
-    if res == errSecSuccess, let encoded = item as? Data, let keys = Data(base64Encoded: encoded) {
+func findKeys(_ encoded: String) -> (Data, Data) {
+    if let keys = Data(base64Encoded: encoded) {
         return (keys[0..<64], keys[64..<(64+32)])
     }
-    else if res == errSecItemNotFound {
-        print("ERROR! Signing key not found. Please run generate_keys tool first.");
-    }
-    else if res == errSecAuthFailed {
-        print("ERROR! Access denied. Can't get keys from the keychain.");
-        print("Go to Keychain Access.app, lock the login keychain, then unlock it again.");
-    }
-    else if res == errSecUserCanceled {
-        print("ABORTED! You've cancelled the request to read the key from the Keychain. Please run the tool again.");
-    } else if res == errSecInteractionNotAllowed {
-        print("ERROR! The operating system has blocked access to the Keychain.");
-    } else {
-        print("ERROR! Unable to access required key in the Keychain", res, "(you can look it up at osstatus.com)");
-    }
+    print("Base64 error")
     exit(1)
 }
 
@@ -56,17 +35,17 @@ func edSignature(data: Data, publicEdKey: Data, privateEdKey: Data) -> String {
 }
 
 let args = CommandLine.arguments;
-if args.count != 2 {
-    print("Usage: \(args[0]) <archive to sign>\nPrivavte EdDSA (ed25519) key is automatically read from the Keychain.\n");
+if args.count != 3 {
+    print("Usage: \(args[0]) <archive to sign> <key>.\n");
     exit(1)
 }
 
-let(priv, pub) = findKeys();
+let(priv, pub) = findKeys(args[2]);
 
 do {
     let data = try Data.init(contentsOf: URL.init(fileURLWithPath: args[1]), options: .mappedIfSafe);
     let sig = edSignature(data:data , publicEdKey: pub, privateEdKey: priv);
-    print("sparkle:edSignature=\"\(sig)\" length=\"\(data.count)\"");
+    print(sig)
 } catch {
     print("ERROR: ", error)
 }
